8253e4c923a9f46ed1042de5cd524c3fa6e0ca5a
rebenew <rebenews@gmail.com>
2025-04-03 20:33:28 -0500

    Se agrego la logica inicial de la subclase SmartThermostat con sus debidos metodos y validaciones

--- src/SmartThermostat.java
+++ src/SmartThermostat.java
@@ -1,2 +1,27 @@
 public class SmartThermostat {
+    private int temperature;
+
+    public SmartThermostat(int initialTemperature) {
+        setTemperature(initialTemperature);
+    }
+
+    public void setTemperature(int temperature) {
+        if (temperature >= 10 && temperature <= 30) {
+            this.temperature = temperature;
+        } else {
+            System.out.println("Temperatura fuera del rango permitido. No se aplicaron cambios.");
+        }
+    }
+    public void increaseTemperature () {
+        setTemperature(this.temperature + 1);
+    }
+    public void decreaseTemperature() {
+        setTemperature(this.temperature - 1);
+    }
+    public boolean isEnergySavingMode() {
+        return this.temperature < 20;
+    }
+    public int getTemperature() {
+        return this.temperature;
+    }
 }
