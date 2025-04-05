f6872e1cdd2538f34e3d8de30688402530f60490
James Orozco <jamesorozcoh@gmail.com>
2025-04-04 22:06:19 -0500

    -attribute value changed on SmartThermostat: boolean instead of String -Logic changed on SmartDevice file with status -Tests made on Main.Java file

--- src/SmartDevice.java
+++ src/SmartDevice.java
@@ -3,34 +3,30 @@ abstract class SmartDevice {
     String model;
     boolean status;
 
-    public SmartDevice(String brand, String model, String status) {
+    public SmartDevice(String brand, String model, boolean status) {
         this.brand = brand;
         this.model = model;
-        this.status = false;
+        this.status = status;
     }
 
     public void turnOn() {
         status = true;
-        System.out.println("The device has been turned on");
+        System.out.println(model + " is now ON");
     }
 
     public void turnOff() {
         status = false;
-        System.out.println("The device has been turned off");
+        System.out.println(model + " is now OFF");
     }
 
     public abstract void deviceInfo();
 
     // Get Smart Device status
-    public boolean getStatus() {
-        return status;
+    public String getStatus() {
+        return status ? "ON" : "OFF";
     }
 
     public void setStatus(boolean status) {
-        if(status) {
-            System.out.println("The Smart device is ON");
-        } else {
-            System.out.println("The smart device is OFF");
-        }
+        this.status = status;
     }
 }
