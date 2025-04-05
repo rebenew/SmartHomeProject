f45229868954cc45f015282273970d73b13fad7f
James Orozco <jamesorozcoh@gmail.com>
2025-04-04 20:05:30 -0500

    updating Smart device file

--- src/SmartDevice.java
+++ src/SmartDevice.java
@@ -1,32 +1,33 @@
 abstract class SmartDevice {
     String brand;
     String model;
-    String status;
+    boolean status;
 
     public SmartDevice(String brand, String model, String status) {
         this.brand = brand;
         this.model = model;
-        setStatus(status);
+        this.status = false;
     }
 
     public void turnOn() {
+        status = true;
         System.out.println("The device has been turned on");
     }
 
     public void turnOff() {
+        status = false;
         System.out.println("The device has been turned off");
     }
 
     public abstract void deviceInfo();
 
     // Get Smart Device status
-    public String getStatus() {
+    public boolean getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
-        if(status.equals("ON")) {
-            this.status = status;
+    public void setStatus(boolean status) {
+        if(status) {
             System.out.println("The Smart device is ON");
         } else {
             System.out.println("The smart device is OFF");
